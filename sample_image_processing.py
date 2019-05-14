0cb019881b1ffec583342d6decbfca5588d569a5
Andrew <andrewespn@hotmail.com>
2019-05-14 16:27:32 -0700

    Videos converted into frames for every folder full of images

--- sample_image_processing.py
+++ sample_image_processing.py
@@ -1,1 +1,21 @@
-import ffmpeg
+import numpy as np
+import os, sys
+
+#create the file location for where the extracted videos into frames go
+def createFolder(folderName):
+    try:
+        if not os.path.exists(folderName):
+            os.makedirs(folderName)
+    except OSError:
+            print("Folder already exists")
+
+
+#converts the videos into frames and places them inside the associated created folder
+def convertVideos(fileLocation, folderName):
+    count = 1
+    for x in os.listdir("./" + fileLocation + "/"):        
+        createFolder("./sample_images/" + folderName + "/video_" + str(count) + "_frames")
+        os.system("ffmpeg -i sample_images/" + folderName + "/" + x + " -r 4 sample_images/" + folderName + "/video_" + str(count) + "_frames/image%05d.jpg -hide_banner")
+        count += 1
+
+convertVideos("./sample_images/tennis_balls", "tennis_balls")
