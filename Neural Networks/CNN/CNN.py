2d6708ec5f6bcc9d97e230c7d3f65be77e3dc639
Andrew <andrewespn@hotmail.com>
2019-08-09 12:33:16 -0700

    Transformed videos into frames to use later

--- Neural Networks/CNN/CNN.py
+++ Neural Networks/CNN/CNN.py
@@ -6,18 +6,7 @@ from keras.datasets import mnist
 import matplotlib.pyplot as plt
 
 batch_size = 32
-epochs = 1000
+epochs = 10
 
-#currently going to form so that I am using handwritten data set provided by keras (adjust later to work for picture data set)
-(trainingData, trainingLabels), (validationData, validationLabels) = mnist.load_data()
-
-trainingLabels = to_categorical(trainingLabels)
-validationLabels = to_categorical(validationLabels)
-
-model = Sequential()
-
-model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(28,28,1)))
-model.add(Conv2D(64, kernel_size=3, activation='relu'))
-model.add(Flatten())
-model.add(Dense(10, activation='softmax'))
+data_dir = "./pms_images"
 
