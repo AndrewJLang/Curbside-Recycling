851fcbbd8025f0355c98668ae0dbc083eab99bf2
Andrew <andrewespn@hotmail.com>
2019-08-10 12:59:55 -0700

    Imported mixed object pms frames, wrote CNN to process multi-class classification

---
+++ Neural Networks/CNN/multi_class_CNN.py
@@ -0,0 +1,94 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, MaxPooling2D
+from keras.utils import to_categorical
+from keras.optimizers import Adam
+from keras.losses import categorical_crossentropy
+from keras.datasets import mnist
+from keras_preprocessing import image
+import matplotlib.pyplot as plt
+import os
+import csv
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+data_dir = "../../pymeanshift/pms_images/"
+
+#shuffle the arrays so they aren't in the same 'linear' order that they are in the csv file
+def shuffle_arrays(arr1, arr2):
+    assert len(arr1) == len(arr2)
+    shuffle_arr1 = np.empty(arr1.shape, dtype=arr1.dtype)
+    shuffle_arr2 = np.empty(arr2.shape, dtype=arr2.dtype)
+    perm = np.random.permutation(len(arr1))
+    for oldIndex, newIndex in enumerate(perm):
+        shuffle_arr1[newIndex] = arr1[oldIndex]
+        shuffle_arr2[newIndex] = arr2[oldIndex]
+    return shuffle_arr1, shuffle_arr2
+
+#Extracts the images and places them in an array
+def processImgs():
+    data = []
+    for x in os.listdir(data_dir):
+        img = image.load_img(data_dir + x, target_size=(400,400))
+        img = image.img_to_array(img)
+        img = img / 255
+        data.append(img)
+    return data
+
+#Extracts the labels for each of the images
+def multiClassImgs():
+    labels = []
+    for x in os.listdir(data_dir):
+        if "plastic" in x:
+            labels.append(0)
+            continue
+        elif "soda" in x:
+            labels.append(1)
+            continue
+        else:
+            labels.append(2)
+            continue
+    return labels
+
+data = np.array(processImgs())
+labels = to_categorical(multiClassImgs())
+data, labels = shuffle_arrays(data,labels)
+labels = to_categorical(labels)
+labels = labels[:, :, 0]
+print(labels.shape)
+split = int(len(data) * 0.8)
+
+trainingData, trainingLabels = data[:split], labels[:split]
+validationData, validationLabels = data[split:], labels[split:]
+
+num_classes = len(labels[0])
+batchSize = 1
+epoch = 10
+learningrate = 0.001
+
+imgShape = trainingData[0].shape
+
+#CNN to run the images through
+model = Sequential()
+model.add(Conv2D(filters=16, kernel_size=3, activation='relu', input_shape=imgShape))
+model.add(MaxPooling2D())
+model.add(Dropout(0.2))
+
+model.add(Conv2D(filters=32, kernel_size=3, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Dropout(0.2))
+
+model.add(Flatten())
+
+model.add(Dense(500, activation='relu'))
+model.add(Dropout(.5))
+
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(optimizer=Adam(lr=learningrate), loss=categorical_crossentropy, metrics=['accuracy'])
+
+model.fit(trainingData, trainingLabels, batch_size=batchSize, epochs=epoch, verbose=1)
+
+_, accuracy = model.evaluate(validationData, validationLabels, verbose=1)
+
+print(f"Model validation accuracy: {accuracy}")
