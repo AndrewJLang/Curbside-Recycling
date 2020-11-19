005bdc8d74c75c58394adebe8e9376af43b1b097
Andrew <andrewespn@hotmail.com>
2020-11-18 18:45:44 -0800

    Renamed file

--- pymeanshift/test_mask.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import os
-
-im = Image.open(os.path.join(os.path.dirname(__file__), "test_image.jpg"))
-pix = im.load()
-
-for x in range(im.size[0]):
-    for y in range(im.size[1]):
-        print(pix[x,y])
